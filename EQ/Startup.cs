b08808c8753b3f32bbbda26fbefe930387ecc0b6
'Konstantinos <'fertakis@gmail.com'>
2016-12-17 15:15:54 +0200

    Add project files.

---
+++ EQ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EQ.Startup))]
+namespace EQ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
